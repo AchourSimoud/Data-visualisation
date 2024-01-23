47505c524d5ff6446a96c64ae9b9ed7d0609ce1a
unknown <achoursimoud@gmail.com>
2024-01-23 10:07:45 +0100

    affichage des bar plot cote a cote + effet hover

--- script.js
+++ script.js
@@ -10,12 +10,19 @@ const coordinates = {
     nantes: [47.218371, -1.553621]
 };
 
+// Définir les dimensions du graphique
+var margin = { top: 40, right: 20, bottom: 20, left: 40 };
+const barPlotDiv = document.querySelector('.bar-plot');
+const width = barPlotDiv.offsetWidth / 2 - margin.left - margin.right;
+const height = barPlotDiv.offsetHeight - margin.top - margin.bottom;
+
 // Initialiser la carte avec Leaflet
 var map = L.map("map").setView(coordinates.lyon, 13);
 
 // Utiliser la surcouche de OpenStreetMap
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "© OpenStreetMap contributors",
+    minZoom: 12,
 }).addTo(map);
 
 L.svg().addTo(map);
@@ -196,164 +203,195 @@ function update() {
         .attr("x", function (d) { return map.latLngToLayerPoint([d.position.latitude, d.position.longitude]).x })
         .attr("y", function (d) { return map.latLngToLayerPoint([d.position.latitude, d.position.longitude]).y });
 }
+var tooltip = d3
+.select("#barChart2")
+.append("div")
+.attr("class", " tooltip");
 
 // Afficher le nombre de pistes par année dans la div avec l'id "plots"
 d3.csv("Data/historique_pistes.csv").then(function (historique) {
-    var selected_graphe = d3.select("#filtre");
-    update_graph(selected_graphe, historique);
-    selected_graphe.on("change", function () {
-        update_graph(selected_graphe, historique);
+  
+     // Agréger les données par année
+     var data = d3.nest()
+     .key(function (d) { return d.anneelivraison; })
+     .rollup(function (v) { return v.length; })
+     .entries(historique);
+
+    // Triez les données par ordre croissant en fonction de la clé
+    data.sort(function (a, b) {
+        return d3.ascending(a.key, b.key);
     });
+    // Filtrer les données pour exclure les clés égales à 0
+    data = data.filter(function (d) {
+        return d.key !== "0";
+    });
+    // Affichage des barres plots
+    var svg = d3.select('#barChart1')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+       
+
+    // Échelle pour l'axe des x
+    var x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
+
+    // Échelle pour l'axe des y
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+
+    // Définir les domaines des échelles en fonction des données
+    x.domain(data.map(function (d) { return d.key; }));
+    y.domain([0, d3.max(data, function (d) { return d.value; })]);
+
+    // Ajouter les barres au graphique
+    svg.selectAll('.bar')
+        .data(data)
+        .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', function (d) { return x(d.key); })
+        .attr('width', x.bandwidth())
+        .attr('y', function (d) { return y(d.value); })
+        .attr('height', function (d) { return height - y(d.value); })
+        .attr('fill', 'steelblue')
+        .on("mousemove", function (d) {
+            // on recupere la position de la souris,
+            d3.select(this).style("cursor", "pointer");
+            var mousePosition = [d3.event.pageX, d3.event.pageY];
+            // on affiche le toolip
+            tooltip
+              .classed("hidden", false)
+              // on positionne le tooltip en fonction
+              // de la position de la souris
+              .attr(
+                "style",
+                "left:" +
+                  (mousePosition[0] + 15) +
+                  "px; top:" +
+                  (mousePosition[1] - 35) +
+                  "px"
+              )
+              // on recupere le nom de l'etat
+              .html(d.value);
+          })
+          .on("mouseout", function () {
+            // on cache le toolip
+            tooltip.classed("hidden", true);
+          });
+
+    // Ajouter l'axe des x
+    svg.append('g')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(d3.axisBottom(x));
+
+    // Ajouter l'axe des y
+    svg.append('g')
+        .call(d3.axisLeft(y));
+
+    // Ajouter un titre au graphique
+    svg.append('text')
+        .attr('x', width / 2)
+        .attr('y', 0 - margin.top / 2)
+        .attr('text-anchor', 'middle')
+        .style('font-size', '16px')
+        .text('Graphique de construction des pistes cyclables à Lyon');
+
+
+// Agréger les données par commune
+    var data = d3.nest()
+        .key(function (d) { return d.commune1; })
+        .rollup(function (v) { return v.length; })
+        .entries(historique);
+
+// Triez les données par ordre croissant en fonction de la clé
+    data.sort(function (a, b) {
+        return d3.ascending(a.value, b.value);
+    });
+
+// Filtrer les données pour exclure les clés vides
+    data = data.filter(function (d) {
+        return d.key !== "";
+    });
+
+// Affichage des barres plots
+    var svg = d3.select('#barChart2')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        
+
+    // Échelle pour l'axe des x
+    var x = d3.scaleLinear()
+        .range([0, width]);
+
+    // Échelle pour l'axe des y
+    var y = d3.scaleBand()
+        .range([height, 0])
+        .padding(0.1);
+
+    // Définir les domaines des échelles en fonction des données
+    x.domain([0, d3.max(data, function (d) { return d.value; })]);
+    y.domain(data.map(function (d) { return d.key; }));
+
+    svg.selectAll('.bar')
+        .data(data)
+        .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('y', function (d) { return y(d.key); })
+        .attr('height', y.bandwidth())
+        .attr('x', 0) // La barre commence à l'extrémité gauche
+        .attr('width', function (d) { return x(d.value); })
+        .attr('fill', 'steelblue')
+        .on("mousemove", function (d) {
+            // on recupere la position de la souris,
+            d3.select(this).style("cursor", "pointer");
+            var mousePosition = [d3.event.pageX, d3.event.pageY];
+            // on affiche le toolip
+            tooltip
+                .classed("hidden", false)
+                // on positionne le tooltip en fonction
+                // de la position de la souris
+                .attr(
+                    "style",
+                    "left:" +
+                    (mousePosition[0] + 15) +
+                    "px; top:" +
+                    (mousePosition[1] - 35) +
+                    "px"
+                )
+                // on recupere le nom de l'etat et la valeur
+                .html(d.key + " " + d.value);
+        })
+          .on("mouseout", function () {
+            // on cache le toolip
+            tooltip.classed("hidden", true);
+          });
+        
+
+        // Ajouter l'axe des x
+        svg.append('g')
+            .attr('transform', 'translate(0,' + height + ')')
+            .call(d3.axisBottom(x));
+
+        // Ajouter l'axe des y
+        svg.append('g')
+            .call(d3.axisLeft(y))
+            .selectAll("text")
+            .style("display", "none");
+        // Ajouter un titre au graphique
+        svg.append('text')
+            .attr('x', width / 2)
+            .attr('y', 0 - margin.top / 2)
+            .attr('text-anchor', 'middle')
+            .style('font-size', '16px')
+            .text('Construction des pistes cyclables dans les communes de Lyon');
+
 });
-
-// Fonction d'affichage des graphiques
-function update_graph(filtre, historique) {
-    d3.select('#plots').html(""); // Effacez le contenu actuel de la div
-
-    if (filtre.property("value") === "annees") {
-        // Définir les dimensions du graphique
-        var margin = { top: 40, right: 20, bottom: 20, left: 40 },
-            width = 800 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
-
-        // Agréger les données par année
-        var data = d3.nest()
-            .key(function (d) { return d.anneelivraison; })
-            .rollup(function (v) { return v.length; })
-            .entries(historique);
-
-        // Triez les données par ordre croissant en fonction de la clé
-        data.sort(function (a, b) {
-            return d3.ascending(a.key, b.key);
-        });
-        // Filtrer les données pour exclure les clés égales à 0
-        data = data.filter(function (d) {
-            return d.key !== "0";
-        });
-        // Affichage des barres plots
-        var svg = d3.select('#plots')
-            .append('svg')
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom)
-            .append('g')
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-            .on("mouseover", function () {
-                d3.select(this).style("cursor", "pointer");
-            });
-
-        // Échelle pour l'axe des x
-        var x = d3.scaleBand()
-            .range([0, width])
-            .padding(0.1);
-
-        // Échelle pour l'axe des y
-        var y = d3.scaleLinear()
-            .range([height, 0]);
-
-        // Définir les domaines des échelles en fonction des données
-        x.domain(data.map(function (d) { return d.key; }));
-        y.domain([0, d3.max(data, function (d) { return d.value; })]);
-
-        // Ajouter les barres au graphique
-        svg.selectAll('.bar')
-            .data(data)
-            .enter().append('rect')
-            .attr('class', 'bar')
-            .attr('x', function (d) { return x(d.key); })
-            .attr('width', x.bandwidth())
-            .attr('y', function (d) { return y(d.value); })
-            .attr('height', function (d) { return height - y(d.value); })
-            .attr('fill', 'steelblue');
-
-        // Ajouter l'axe des x
-        svg.append('g')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(d3.axisBottom(x));
-
-        // Ajouter l'axe des y
-        svg.append('g')
-            .call(d3.axisLeft(y));
-
-        // Ajouter un titre au graphique
-        svg.append('text')
-            .attr('x', width / 2)
-            .attr('y', 0 - margin.top / 2)
-            .attr('text-anchor', 'middle')
-            .style('font-size', '16px')
-            .text('Graphique de construction des pistes cyclables à Lyon');
-    } else if (filtre.property("value") === "communes") {
-        // Définir les dimensions du graphique
-        var margin = { top: 40, right: 20, bottom: 20, left: 150 },
-            width = 600 - margin.left - margin.right,
-            height = 600 - margin.top - margin.bottom;
-
-        // Agréger les données par commune
-        var data = d3.nest()
-            .key(function (d) { return d.commune1; })
-            .rollup(function (v) { return v.length; })
-            .entries(historique);
-
-        // Triez les données par ordre croissant en fonction de la clé
-        data.sort(function (a, b) {
-            return d3.ascending(a.value, b.value);
-        });
-
-        // Filtrer les données pour exclure les clés vides
-        data = data.filter(function (d) {
-            return d.key !== "";
-        });
-
-        // Affichage des barres plots
-        var svg = d3.select('#plots')
-            .append('svg')
-            .attr('width', width + margin.left + margin.right)
-            .attr('height', height + margin.top + margin.bottom)
-            .append('g')
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-        // Échelle pour l'axe des x
-        var x = d3.scaleLinear()
-            .range([0, width]);
-
-        // Échelle pour l'axe des y
-        var y = d3.scaleBand()
-            .range([height, 0])
-            .padding(0.1);
-
-        // Définir les domaines des échelles en fonction des données
-        x.domain([0, d3.max(data, function (d) { return d.value; })]);
-        y.domain(data.map(function (d) { return d.key; }));
-
-        svg.selectAll('.bar')
-            .data(data)
-            .enter().append('rect')
-            .attr('class', 'bar')
-            .attr('y', function (d) { return y(d.key); })
-            .attr('height', y.bandwidth())
-            .attr('x', 0) // La barre commence à l'extrémité gauche
-            .attr('width', function (d) { return x(d.value); })
-            .attr('fill', 'steelblue');
-
-        // Ajouter l'axe des x
-        svg.append('g')
-            .attr('transform', 'translate(0,' + height + ')')
-            .call(d3.axisBottom(x));
-
-        // Ajouter l'axe des y
-        svg.append('g')
-            .call(d3.axisLeft(y));
-
-        // Ajouter un titre au graphique
-        svg.append('text')
-            .attr('x', width / 2)
-            .attr('y', 0 - margin.top / 2)
-            .attr('text-anchor', 'middle')
-            .style('font-size', '16px')
-            .text('Graphique de construction des pistes cyclables à Lyon');
-    }
-
-
-}
 
 function showBarChart(mechanicalBikes, electricalBikes, stands, capacity) {
     document.getElementById("affichage").innerHTML += "<br><br><strong><span style='font-size: 15px;'>Ratio type de vélos disponibles : </span></strong><br><div id='barChart'></div>";
